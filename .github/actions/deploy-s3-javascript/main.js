2bddfb78098eedcc77f1f0ba9333e75d315ff149
surender <agni1984@gmail.com>
2025-05-30 22:09:14 +0530

    fix5

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -12,11 +12,7 @@ function run(){
    
     // upload files to S3
     const s3Uri=`s3://${bucket}`
-    exec.exec(`aws s3 sync
-        ${distFolder},
-        ${s3Uri} --region ${bucketRegion}`
-     );
-    core.notice('Hello from my custom JavaScript Action!');
+    exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`);
 }
 
 run();
