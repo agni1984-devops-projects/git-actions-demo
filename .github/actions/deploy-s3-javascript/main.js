48dd56485d8eb7a9bd22927d5614cb8d94e2f0ed
surender <agni1984@gmail.com>
2025-05-30 22:04:51 +0530

    Custom Deploy action

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -4,6 +4,18 @@ const exec=require('@actions/exec');
 
 
 function run(){
+    // Get some inputs from the action
+    const bucket=core.getInput('bucket',{required:true});
+    const bucketRegion=core.getInput('bucket-region',{required:true});
+    const distFolder=core.getInput('dist-folder',{required:true});
+    github.context.log(`Deploying to bucket: ${bucket} in region: ${bucketRegion} from folder: ${distFolder}`);
+   
+    // upload files to S3
+    const s3Uri=`s3://${bucket}`
+    exec.exec(`aws s3 sync
+        ${distFolder},
+        ${s3Uri} --region ${bucketRegion}`
+     );
     core.notice('Hello from my custom JavaScript Action!');
 }
 
